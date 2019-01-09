d243696cf774c559a8e15fd49518e5e9511e7ec8
Berkmann18 <maxieberkmann@gmail.com>
2019-01-09 15:47:50 +0000

    New(Test): Added an `express` focused test suite
    Not sure how I could improve the coverage given what the other lines are.
    
    re #71

---
+++ test/express.test.js
@@ -0,0 +1,19 @@
+const Server = require('../index');
+/* eslint-disable node/no-unpublished-require */
+const { expect } = require('chai'),
+  getPort = require('get-port'),
+  express = require('express');
+/* eslint-enable node/no-unpublished-require */
+
+describe('Simply Express', () => {
+  it('should run well', (done) => {
+    getPort().then(port => {
+      const app = express();
+      app.set('env', 'test');
+      const server = new Server(app, port, { gracefulClose: false, silent: true });
+      expect(typeof server).to.equal('object');
+    }, err => console.error('getPort error:', err))
+      .catch(err => console.error('HTTPS test error:', err))
+      .then(done)
+  });
+});
